66a6ddbfc25e1b2ec7756fc0f7e222f6eaa1905f
Hendrick <hendrickwijaya21@gmail.com>
2021-02-23 20:16:00 +0700

    Adding new route and file

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcomer');
+    // return 'Hendrick';
+});
+Route::get('/test', function () {
+    return view('test');
 });
